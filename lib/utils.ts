52c4a623d3cece32ce488770aeb28b88461833fe
aniketdebnath <rkstaraniket34@gmail.com>
2025-02-27 02:57:14 +1100

    sign-in sign-up sign-out functionality with custom error handling

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,3 +14,27 @@ export function formatNumberWithDecimals(num: number): string {
   if (isNaN(num) || !isFinite(num)) return "0.00";
   return num.toFixed(2);
 }
+
+//Format errors
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function formatError(error: any) {
+  if (error.name === "ZodError") {
+    //Zod Error handling
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    );
+    return fieldErrors.join(". ");
+  } else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    //Prisma Error handling
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
+  } else {
+    //Generic Error handling
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error);
+  }
+}
